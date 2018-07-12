43a9e58f466d7b875594893651f0a142fb90863e
Pete Whittle <peterwhittle75@gmail.com>
2018-07-12 20:14:31 +0100

    created advert class + constructor

--- src/main/java/models/Advert.java
+++ src/main/java/models/Advert.java
@@ -1,4 +1,25 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Advert {
+
+    private int id;
+    private String advertTitle;
+    private String advertDescription;
+//    private ArrayList<Categories> categories;
+    private Double askingPrice;
+    private User advertOwner;
+
+    public Advert(String advertTitle, String advertDescription, Double askingPrice, User advertOwner) {
+        this.advertTitle = advertTitle;
+        this.advertDescription = advertDescription;
+//        this.categories = new ArrayList<Categories>();
+        this.askingPrice = askingPrice;
+        this.advertOwner = advertOwner;
+    }
+
+
+
+
 }
