ac4a25370be803f13b7933061d6403fd585783ab
Pete Whittle <peterwhittle75@gmail.com>
2018-07-12 19:45:19 +0100

    Added constructor, getters & setters

--- src/main/java/models/ShopFront.java
+++ src/main/java/models/ShopFront.java
@@ -1,7 +1,22 @@
 package models;
 
+import java.util.ArrayList;
+
 public class ShopFront {
 
+    private ArrayList<User> allUsers;
+    private ArrayList<Advert> allAdverts;
 
+    public ShopFront() {
 
+        this.allUsers = new ArrayList<User>();
+        this.allAdverts = new ArrayList<Advert>();
+
+    }
+
+    public ArrayList<User> getAllUsers() {return allUsers;}
+    public void setAllUsers(ArrayList<User> allUsers) {this.allUsers = allUsers;}
+
+    public ArrayList<Advert> getAllAdverts() {return allAdverts;}
+    public void setAllAdverts(ArrayList<Advert> allAdverts) {this.allAdverts = allAdverts;}
 }
