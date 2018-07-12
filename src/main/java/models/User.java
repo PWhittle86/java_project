27863b15f983d94bccaa47a9cbc29b4e2257a1d8
Pete Whittle <peterwhittle75@gmail.com>
2018-07-12 21:19:58 +0100

    added annotations to user class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,7 +1,10 @@
 package models;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name= "users")
 public class User {
 
     private int id;
@@ -13,12 +16,17 @@ public class User {
         this.userAdverts = new ArrayList<Advert>();
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     public int getId() {return id;}
     public void setId(int id) {this.id = id;}
 
+    @Column(name = "username")
     public String getUsername() {return username;}
     public void setUsername(String username) {this.username = username;}
 
+    @OneToMany(mappedBy = "user_id", fetch= FetchType.LAZY)
     public ArrayList<Advert> getUserAdverts() {return userAdverts;}
     public void setUserAdverts(ArrayList<Advert> userAdverts) {this.userAdverts = userAdverts;}
 
