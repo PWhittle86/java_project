f8ada6b8be72c0a8f308af01f82263280d04a1b7
Pete Whittle <peterwhittle75@gmail.com>
2018-07-13 13:32:18 +0100

    Created show all adds view

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -3,7 +3,14 @@ package controllers;
 import db.Seeds;
 
 public class MainController {
+
     public static void main(String[] args) {
         Seeds.seedData();
+
+        AdController adController = new AdController();
+
     }
+
+
+
 }
