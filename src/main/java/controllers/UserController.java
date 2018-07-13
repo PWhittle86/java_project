c7e51ecf5aa2f704c559b35cf26c02283a56fb24
Jack Murning <jackrichardmurning@gmail.com>
2018-07-13 17:51:12 +0100

    add create new user fnality

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class UserController {
 
@@ -24,6 +25,13 @@ public class UserController {
     }
 
     private void setupEndPoints(){
+
+        get("/users/new", (req, res)->{
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/users/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
         get("/users", (req, res)->{
             Map<String, Object> model = new HashMap<>();
             List<User> allUsers = DBHelper.getAll(User.class);
@@ -32,17 +40,25 @@ public class UserController {
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
+
         get("/users/:id", (req, res)->{
             Map<String, Object> model = new HashMap<>();
             String strId = req.params(":id");
             Integer id = Integer.parseInt(strId);
             User foundUser = DBHelper.find(id, User.class);
-
             List<Advert> userAdverts = DBUser.getUsersAdverts(foundUser);
             model.put("foundUser", foundUser);
             model.put("userAdverts", userAdverts);
             model.put("template", "templates/users/view.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/users",(req, res)->{
+            String username = req.queryParams("username");
+            User newUser = new User(username);
+            DBHelper.save(newUser);
+            res.redirect("/users");
+            return null;
         }, new VelocityTemplateEngine());
     }
 
