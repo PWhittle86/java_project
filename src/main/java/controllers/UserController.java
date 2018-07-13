d3ff706a78319e5332bea4ab474be86dbf60cac0
Jack Murning <jackrichardmurning@gmail.com>
2018-07-13 19:03:31 +0100

    add working update view for users

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -53,10 +53,32 @@ public class UserController {
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
+        get("/users/:id/edit", (req, res)-> {
+            String strId = req.params(":id");
+            Integer id = Integer.parseInt(strId);
+            User foundUser = DBHelper.find(id, User.class);
+            Map<String, Object> model = new HashMap<>();
+            model.put("foundUser", foundUser);
+            model.put("template", "templates/users/update.vtl");
+            return  new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
         post("/users",(req, res)->{
             String username = req.queryParams("username");
             User newUser = new User(username);
             DBHelper.save(newUser);
+            res.redirect("/users");
+            return null;
+        }, new VelocityTemplateEngine());
+
+        post("users/:id", (req, res)->{
+            String strId = req.params(":id");
+            Integer id = Integer.parseInt(strId);
+            User user = DBHelper.find(id, User.class);
+            String username = req.queryParams("username");
+
+            user.setUsername(username);
+            DBHelper.save(user);
             res.redirect("/users");
             return null;
         }, new VelocityTemplateEngine());
