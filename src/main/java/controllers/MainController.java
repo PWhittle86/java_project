d54b4d6bf2589d4a31f0cc2cd45f128462b5d0c8
Jack Murning <jackrichardmurning@gmail.com>
2018-07-17 09:15:01 +0100

    add more styling to nav bars

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -2,9 +2,13 @@ package controllers;
 
 import db.Seeds;
 
+import static spark.SparkBase.staticFileLocation;
+
 public class MainController {
 
     public static void main(String[] args) {
+        staticFileLocation("public");
+
         Seeds.seedData();
 
         AdController adController = new AdController();
