c3fd90f60d7ba83220cc61d598209e9ca163a37b
Pete Whittle <peterwhittle75@gmail.com>
2018-08-16 20:07:18 +0100

    Heroku upload

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -2,20 +2,31 @@ package controllers;
 
 import db.Seeds;
 
+import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 public class MainController {
 
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+
         staticFileLocation("public");
 
         Seeds.seedData();
 
         AdController adController = new AdController();
         UserController userController =new UserController();
-
     }
 
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
 
 
 }
